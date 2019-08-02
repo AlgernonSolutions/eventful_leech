7218d83c7201a1e1cbfdb5328bc995a952fba77b
Jeff <jcubeta@algernon.solutions>
2019-08-02 17:41:00 -0400

    added leech variable to handle packaging for SFN

--- src/toll_booth/tasks/__init__.py
+++ src/toll_booth/tasks/__init__.py
@@ -2,3 +2,4 @@ from toll_booth.tasks.leech import leech
 from toll_booth.tasks.push_graph import push_graph
 from toll_booth.tasks.push_index import push_index
 from toll_booth.tasks.push_s3 import push_s3
+from toll_booth.tasks.push_event import push_event
